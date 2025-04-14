50375f3d55046490719bfcf086e3297e81f669b9
SebastianDuarteTens <sebastianduarte@tensorcol.com>
2025-04-14 14:29:36 -0500

    fix(ui): adjustment responsive design

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,68 @@
-<script setup>
-//import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div Class="logo">
-      <img src="/SGI.svg" alt="Seafloo Geotech" />
+  <div class="container">
+    <div class="logo">
+      <img src="/SGI.svg" alt="Seafloor Geotech" />
+    </div>
+    <p>Coming Soon...</p>
   </div>
-  <p>Coming Soon...</p>
 </template>
 
 <style scoped>
-p {
-  font-size: 2rem;
-  color: #666;
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100%;
+  width: 100%;
 }
 
 .logo {
-  width: 400px;
+  width: 100%;
+  max-width: 400px;
   height: auto;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
+  margin: 0 auto;
 }
 
-.logo:hover {
-  filter: drop-shadow(0 0 2em #9c9c9c3a);
+.logo img {
+  width: 100%;
+  height: auto;
+}
+
+p {
+  font-size: 2rem;
+  color: #666;
+  margin-top: 20px;
+}
+
+
+@media (max-width: 768px) {
+  p {
+    font-size: 1.5rem;
+  }
+
+  .logo {
+    max-width: 300px;
+    padding: 1em;
+  }
+}
+
+@media (max-width: 480px) {
+  p {
+    font-size: 1.2rem;
+  }
+
+  .logo {
+    max-width: 250px;
+    padding: 0.8em;
+  }
 }
 </style>
+
 
 <style>
 @import url(./style.css);
