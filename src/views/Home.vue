d06a045d854c6f42a2c00749bc1da9b3bc3c40e3
SebastianDuarteTens <sebastianduarte@tensorcol.com>
2025-04-29 15:08:04 -0500

    fix(ui): home adjustment

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-hero row items-center">
     <div class="column q-pl-xl"
-      style="max-width: 1200px; margin-top: 25vh; margin-left: 50vh; width: 100%; height: 100%;">
+      style="max-width: 1200px; margin-top: 25vh; width: 100%; height: 100%; align-items: center;">
       <img src="../assets/SGI.svg" alt="Logo" class="hero-logo" />
       <p class="hero-text">Preparing the future with<br /><span class="highlight">advanced geotechnical</span><br /><span class="highlight">solutions</span> that
         guarantee<br /><span class="highlight">efficient, sustainable, and<br />secure projects.</span></p>
