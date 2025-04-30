eed16ec9d00b634d0ab3c2dc9acb167a1ae2e64b
SebastianDuarteTens <sebastianduarte@tensorcol.com>
2025-04-30 08:15:33 -0500

    fix(ui): adjustment text equipment

--- src/views/Equipment.vue
+++ src/views/Equipment.vue
@@ -68,7 +68,7 @@ const slides = [
     background: `linear-gradient(rgba(124, 11, 19, 0.9), rgba(124, 11, 19, 0.9)), url(${Slide3})`,
     thumbnail: ico3,
     label: 'Rossfelder p-5',
-    subtitle: 'ROSSFELDER P-5',
+    subtitle: 'ROSSFELDER P-5 VIBROCORE',
     description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.'
   }
 ]
